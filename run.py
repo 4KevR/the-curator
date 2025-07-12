2c9b7fd05af1a73430108b5220e069c9490ae5e8
Fabian Aisenbrey <unlfv@student.kit.edu>
2025-07-12 17:11:10 +0200

    Changes in meeting

--- run.py
+++ run.py
@@ -4,4 +4,4 @@ from src.backend.application import app, socketio
 
 if __name__ == "__main__":
     logging.getLogger().info("Running Flask-SocketIO app...")
-    socketio.run(app, host="127.0.0.1", port=5000)
+    socketio.run(app, host="127.0.0.1", port=5000, allow_unsafe_werkzeug=True)
