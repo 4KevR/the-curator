bfacad1d8f75aa411d005a5e0243a056aea1d767
Fabian Aisenbrey <unlfv@student.kit.edu>
2025-07-13 22:42:57 +0200

    Script to evaluate Question Answering

---
+++ scripts/evaluate_question_answering.py
@@ -0,0 +1,144 @@
+#
+#
+#
+# =============================  VARIABLES  ========================================================================
+
+# options: 'local_llama', 'kit_llama', 'kit_llama_req', 'local_qwen8', 'local_qwen14'
+llms_to_use: str = "local_llama"
+
+# Where are the audio files? If asr should be skipped (only using text prompts), set to None.
+audio_file_path: str | None = None  # "./data/recording_data/fabian"
+
+# options: 'local_whisper_medium', 'lecture_translator'
+asr_to_use: str = "local_whisper_medium"
+
+default_temperature: float = 0.0
+default_max_tokens: int = 1000
+
+# ==================================================================================================================
+
+print(
+    f"""The configuration is:
+llms_to_use: {llms_to_use}
+audio_file_path: {audio_file_path}
+asr_to_use: {asr_to_use}
+default_temperature: {default_temperature}
+default_max_tokens: {default_max_tokens}
+"""
+)
+
+import time  # noqa E402
+
+script_start_time = time.time()
+
+import logging  # noqa E402
+import os  # noqa E402
+import sys  # noqa E402
+import warnings  # noqa E402
+from datetime import datetime  # noqa E402
+from zoneinfo import ZoneInfo  # noqa E402
+
+import pandas as pd  # noqa E402
+from dotenv import load_dotenv  # noqa E402
+
+# test that the current working dir is "the-curator"
+if os.path.basename(os.path.abspath(".")) != "the-curator":
+    raise RuntimeError("This script must be run from the 'the-curator' directory.")
+
+# must happen before project imports!
+if not (os.path.exists(".env") and os.path.exists(".env.db")):
+    raise FileNotFoundError("No .env or .env.db found.")
+
+load_dotenv(".env")
+load_dotenv(".env.db")
+
+logging.basicConfig(level=logging.WARN)
+
+# Suppress FutureWarning from transformers
+warnings.filterwarnings("ignore", category=FutureWarning)
+logging.getLogger("transformers").setLevel(logging.ERROR)
+
+from src.backend.modules.asr.cloud_lecture_translator import CloudLectureTranslatorASR  # noqa E402
+from src.backend.modules.asr.local_whisper_asr import LocalWhisperASR  # noqa E402
+from src.backend.modules.evaluation.load_test_data.load_test_data import load_test_data  # noqa E402
+from src.backend.modules.evaluation.run_tests.evaluation_pipeline import EvaluationPipeline  # noqa E402
+from src.backend.modules.llm.kit_llm import KitLLM  # noqa E402
+from src.backend.modules.llm.kit_llm_req import KitLLMReq  # noqa E402
+from src.backend.modules.llm.lm_studio_llm import LMStudioLLM  # noqa E402
+
+if llms_to_use == "local_llama":
+    task_llm = LMStudioLLM("meta-llama-3.1-8b-instruct", default_temperature, default_max_tokens)
+    comparison_llm = LMStudioLLM("meta-llama-3.1-8b-instruct", 0.0, 10)
+elif llms_to_use == "kit_llama":
+    task_llm = KitLLM(0.001, default_max_tokens)
+    comparison_llm = KitLLM(0.001, 10)
+elif llms_to_use == "kit_llama_req":
+    task_llm = KitLLMReq(os.getenv("LLM_URL"), 0.001, default_max_tokens)
+    comparison_llm = KitLLMReq(os.getenv("LLM_URL"), 0.001, 10)
+elif llms_to_use == "local_qwen8":
+    task_llm = LMStudioLLM("qwen3-8b", default_temperature, default_max_tokens, no_think=True)
+    comparison_llm = LMStudioLLM("qwen3-8b", 0.0, 20, no_think=True)  # needs more tokens for empty thinking block.
+elif llms_to_use == "local_qwen14":
+    task_llm = LMStudioLLM("qwen3-14b", default_temperature, default_max_tokens, no_think=True)
+    comparison_llm = LMStudioLLM("qwen3-14b", 0.0, 20, no_think=True)
+else:
+    raise ValueError(f"Unknown llm_to_use: {llms_to_use}")
+
+if asr_to_use == "local_whisper_medium":
+    asr = LocalWhisperASR("openai/whisper-medium")
+elif asr_to_use == "lecture_translator":
+    asr = CloudLectureTranslatorASR()
+else:
+    raise ValueError(f"Unknown asr_to_use: {asr_to_use}")
+
+
+now = datetime.now(ZoneInfo("Europe/Berlin")).strftime("%Y-%m-%d %H:%M:%S %z")
+log_file_path = f"data/logs/{now} q-a evaluation_log.json"
+print(f"Log file path will be '{log_file_path}'")
+
+eval_pipeline = EvaluationPipeline(
+    asr=asr,
+    task_llm=task_llm,
+    fuzzy_matching_llm=comparison_llm,
+    llm_judge=comparison_llm,
+    audio_recording_dir_path=audio_file_path,
+    verbose_task_execution=False,
+    print_progress=True,
+    log_file_path=log_file_path,
+)
+
+print(f"Startup took {time.time() - script_start_time:.2f} seconds.\n")
+
+# =============================  STARTUP FINISHED  =================================================================
+
+
+tests = load_test_data("tests/data/tests.json")
+
+# get question-answering tests
+question_answering_tests = tests.question_answering[:]
+
+print(f"Tests loaded after: {time.time() - script_start_time:.2f} seconds.\n")
+
+# here the tests are run:
+QA_RES = eval_pipeline.evaluate_individual_tests(question_answering_tests)
+
+print("\nAll tests executed.\n")
+
+print(f"Total test duration: {sum(it.time_taken_s for it in QA_RES):.2f} seconds.\n")
+
+total_prompt_tokens = sum(res.token_usage.prompt_tokens for res in QA_RES if res.token_usage)
+total_completion_tokens = sum(res.token_usage.completion_tokens for res in QA_RES if res.token_usage)
+total_tokens = total_prompt_tokens + total_completion_tokens
+
+print("Cumulative Token Usage:")
+print(f"  Prompt Tokens: {total_prompt_tokens}")
+print(f"  Completion Tokens: {total_completion_tokens}")
+print(f"  Total Tokens: {total_tokens}\n")
+
+tmp = pd.DataFrame()
+raw = ["crashed" if r.crashed else ("passed" if r.passed else "failed") for r in QA_RES]
+tmp["abs"] = (pd.Series(raw + ["crashed", "passed", "failed"]).value_counts() - 1).sort_index()
+tmp["rel"] = (tmp["abs"] / sum(tmp["abs"]) * 100).round(2)
+
+print(tmp)
+print("\nFinished!")
